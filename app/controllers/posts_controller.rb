d4866e278f5fcc25e97cad48e4d7f706d70538ff
Dimtar Dimtrov <bmwf6@abv.gb>
2017-08-22 22:30:05 +0300

    Added kaminari paggination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
+    @posts = Post.all.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def show
